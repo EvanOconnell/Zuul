570feaf844946896ded5d060b9854a70d9c7f5e1
EvanOconnell <email.evan.oconnell@gmail.com>
2013-02-07 11:28:24 -0800

    Added packaging to fix import errors with Room.Direction enum.
    I could have just used Room.Direction.NORTH, etc. but this is much more
    convenient. Also added jardesc to make quick exporting easier (for
    eclipse)

---
+++ src/Zuul/CommandWords.java
@@ -0,0 +1,41 @@
+package Zuul;
+/*
+ * This class is the main class of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ * 
+ * This class holds an enumeration of all command words known to the game.
+ * It is used to recognise commands as they are typed in.
+ *
+ * @version 1.1 (1/17/13)
+ */
+
+class CommandWords
+{
+    // a constant array that holds all valid command words
+    private static final String validCommands[] = {
+        "go", "quit", "help", "look", "eat"
+    };
+
+    /**
+     * Constructor - initialise the command words.
+     */
+    public CommandWords()
+    {
+        // nothing to do at the moment...
+    }
+
+    /**
+     * Check whether a given String is a valid command word. 
+     * Return true if it is, false if it isn't.
+     */
+    public boolean isCommand(String aString)
+    {
+        for(int i = 0; i < validCommands.length; i++) {
+            if(validCommands[i].equals(aString))
+                return true;
+        }
+        // if we get here, the string was not found in the commands
+        return false;
+    }
+
+}
---
+++ src/Zuul/Game.java
@@ -0,0 +1,237 @@
+package Zuul;
+
+import Zuul.Room.Direction;
+
+/**
+ * 
+ *  To play this game, create an instance of this class and call the "play"
+ *  method.
+ *  
+ *  NEW CHANGES - 1/17/13 Evan O'Connell:
+ *  Added workshop, basement, hallway and vault. Workshop seems like just 
+ *  another room, until you remember that there's always been that basement
+ *  used for storage there. When you look in the basement, you see a hallway
+ *  you've never seen before. The dark hallway leads to the vault full of
+ *  old pieces of technology and relics of the past the school has saved.
+ *  
+ *  
+ *  
+ * -------
+ * 
+ * @version 3 (02.06.2013) 
+ * @author Evan O'Connell
+ * 
+ * Commits and repo can be seen at http://github.com/EvanOconnell/Zuul.git
+ */
+
+class Game 
+{
+    private final Parser parser;
+    private Room currentRoom;
+        
+    /**
+     * Create the game and initialise its internal map.
+     */
+    public Game() 
+    {
+        createRooms();
+        parser = new Parser();
+    }
+    
+    /**
+     * This is used for testing in jar form or eclipse.
+     */
+    public static void main(String[] args){
+        Game game = new Game();
+        game.play();
+    }
+
+    /**
+     * Create all the rooms and link their exits together.
+     */
+    private void createRooms()
+    {
+        Room outside, theatre, pub, lab, office, workshop, basement, hallway, vault;
+      
+        // create the rooms
+        outside = new Room("outside the main entrance of the university");
+        theatre = new Room("in a lecture theatre");
+        pub = new Room("in the campus pub");
+        lab = new Room("in a computing lab");
+        office = new Room("in the computing admin office");
+        workshop = new Room("in the craft/wood workshop");
+        basement = new Room("in the workshop's storage Basement");
+        hallway = new Room("in a dark hallway out of the basement you've never seen before...");
+        vault = new Room("in a wonderful vault filled with old equipment, materials, and technological relics from years ago.");
+        
+//        outside.setExits(null, theatre, lab, pub, null, null);
+//        theatre.setExits(null, null, null, outside, null, null);
+//        pub.setExits(null, outside, null, null, null, null);
+//        lab.setExits(outside, office, null, null, null, null);
+//        office.setExits(null, null, workshop, lab, null, null);
+//        workshop.setExits(office, null, null, null, null, basement);
+//        basement.setExits(hallway, null, null, null, workshop, null);
+//        hallway.setExits(vault, null, basement, null, null, null);
+//        vault.setExits(null, null, hallway, null, null, null);
+        
+        outside.setExit(theatre, Direction.EAST);
+        outside.setExit(lab, Direction.SOUTH);
+        outside.setExit(pub, Direction.WEST);
+        theatre.setExit(outside, Direction.WEST);
+        pub.setExit(outside, Direction.EAST);
+        lab.setExit(outside, Direction.NORTH);
+        lab.setExit(office, Direction.EAST);
+        office.setExit(workshop, Direction.SOUTH);
+        office.setExit(lab, Direction.WEST);
+        workshop.setExit(office, Direction.NORTH);
+        workshop.setExit(basement, Direction.DOWN);
+        basement.setExit(hallway, Direction.NORTH);
+        basement.setExit(workshop, Direction.UP);
+        hallway.setExit(vault, Direction.NORTH);
+        hallway.setExit(basement, Direction.SOUTH);
+        vault.setExit(hallway, Direction.SOUTH);
+        
+        currentRoom = outside;
+    }
+    
+    /**
+     *  Main play routine.  Loops until end of play.
+     */
+    public void play() 
+    {            
+        printWelcome();
+
+        // Enter the main command loop.  Here we repeatedly read commands and
+        // execute them until the game is over.
+                
+        boolean finished = false;
+        while (! finished) {
+            Command command = parser.getCommand();
+            finished = processCommand(command);
+        }
+        System.out.println("Thank you for playing.  Good bye.");
+    }
+
+    /**
+     * Print out the opening message for the player.
+     */
+    private void printWelcome()
+    {
+        System.out.println("\nWelcome to Adventure!" +
+            "\nAdventure is a new, incredibly boring adventure game." +
+            "\nType 'help' if you need help.\n"
+        );
+        printRoomDescription();
+    }
+    
+    /**
+     * Retrieves and prints description info from currentRoom object. 
+     */
+    private void printRoomDescription(){
+        System.out.println(currentRoom.getLongDescription());
+    }
+
+    /**
+     * Given a command, process (that is: execute) the command.
+     * If this command ends the game, true is returned, otherwise false is
+     * returned.
+     */
+    private boolean processCommand(Command command) 
+    {
+        boolean wantToQuit = false;
+
+        if(command.isUnknown()) {
+            System.out.println("I don't know what you mean...");
+            return false;
+        }
+        
+        String commandWord = command.getCommandWord();
+        if(commandWord.equalsIgnoreCase("help")){
+        	printHelp();
+        }
+        if(commandWord.equalsIgnoreCase("go")){
+        	goRoom(command);
+        }
+        if(commandWord.equalsIgnoreCase("quit")){
+        	wantToQuit = quit(command);
+        }
+        if(commandWord.equalsIgnoreCase("look")){
+        	look();
+        }
+        if(commandWord.equalsIgnoreCase("eat")){
+        	eat();
+        }
+
+        return wantToQuit;
+    }
+
+    // implementations of user commands:
+
+    /**
+     * Print out some help information.
+     * Here we print some stupid, cryptic message and a list of the 
+     * command words.
+     */
+    private void printHelp() 
+    {
+        System.out.println("You are lost. You are alone. You wander" +
+                "\naround at the university.\n" +
+                "\nYour command words are:\n"
+            );
+        System.out.println("\tgo\t|\tquit\t|\thelp");
+    }
+    
+    /** Look around the room.
+     * 
+     * Prints room description.
+     */
+    private void look(){
+    	printRoomDescription();
+    }
+    
+    /**
+     * Restores food level. 
+     */
+    private void eat(){
+    	System.out.println("You have eaten now and you are not hungry anymore!");
+    }
+
+    /** 
+     * Try to go to one direction. If there is an exit, enter
+     * the new room, otherwise print an error message.
+     */
+    private void goRoom(Command command) 
+    {
+        if(!command.hasSecondWord()) {
+            // if there is no second word, we don't know where to go...
+            System.out.println("Go where?");
+            return;
+        }
+        Room.Direction dir = Room.Direction.parse(command.getSecondWord());
+        
+        Room nextRoom = null;
+        
+        if(!currentRoom.hasExit(dir)){
+            System.out.println("There is no exit that way!");
+        } else {
+            nextRoom = currentRoom.getExit(dir);
+            currentRoom = nextRoom;
+            printRoomDescription();
+        }
+        
+    }
+
+    /** 
+     * "Quit" was entered. Check the rest of the command to see
+     * whether we really quit the game. Return true, if this command
+     * quits the game, false otherwise.
+     */
+    private boolean quit(Command command) 
+    {
+        if(command.hasSecondWord()){
+            System.out.println("Quit what?");
+            return false;
+        }
+        return true;  // signal that we want to quit
+    }
+}
