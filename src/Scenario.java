a88f3c885077af13584887d53904b411798a2725
EvanOconnell <email.evan.oconnell@gmail.com>
2013-02-23 22:32:25 -0800

    Added Scenario class

---
+++ src/Scenario.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Represents a game scenario including connected rooms and items
+ * 
+ * @author Tang
+ * @version 2013.02.16
+ */
+public class Scenario
+{
+    private ArrayList<Room> rooms;
+    private Room startRoom;
+    private Random random;
+
+    /**
+     * Constructor for objects of class Scenario
+     */
+    public Scenario()
+    {
+//        random = new Random();
+        
+        
+    }
+
+//    /**
+//     * @return  the start room for this scenario
+//     */
+//    public Room getStartRoom(){
+//        
+//    }
+//    
+//    /**
+//     * @return  a random room from this scenario
+//     */
+//    public Room getRandomRoom(){
+//        
+//    }
+}
